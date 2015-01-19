dcf86c16cb69ff19a9a0d035f2b3c91fa22df749
Tafro, Leonard <Leonard.Tafro@mccann.com>
2015-01-19 01:07:40 -0500

    Initial build of site - bower, bootstrap, etc

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+console.log('Starting TimJacksonTalks web');
+// $(function() {
+// 	'use strict';
+//     var BV = new $.BigVideo({useFlashForFirefox:false});
+// 	BV.init();
+//     BV.show([
+//         { type: 'video/ogg',  src: 'video/WebSite Montage-HD.mp4' }
+//     ]);
+// });
+
+
+
+
